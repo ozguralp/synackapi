ae1a5d5f31c45cdb20443c0a9c9cb490d120a5b5
Ozgur Alp <ozgurralp@gmail.com>
2020-04-10 13:34:40 +0300

    Add files via upload

--- hydra_export.py
+++ hydra_export.py
@@ -13,14 +13,17 @@ headers = {"Authorization": "Bearer "+auth_header}
 iterator = 1
 next_page = True
 results = [[],[]]
+results_final = []
 
 while next_page:
     try:
         url = SYNACK_API_URL + HYDRA_TARGET + "?page="+str(iterator)+"&listing_uids="+target_codename+"&q=%2Bport_is_open%3Atrue"
         target_response = requests.get(url, headers=headers).json()
         for i in range (len(target_response)): 
-            results[0].append((target_response[i]["ip"]))
-            results[1].append((str(target_response[i]["ports"])).split("'")[1])
+            ports = str(target_response[i]["ports"]).split("}}}}")
+            for j in range (len(ports)-1):
+                results[0].append((target_response[i]["ip"]))
+                results[1].append(str(ports[j]).split("'")[1])
         print("Page "+str(iterator)+" done.")
         if len(target_response) < 10:
             print("Done loading target.")
@@ -31,4 +34,8 @@ while next_page:
         next_page = False
 
 for i in range (len(results[0])): 
-    print str(results[0][i])+":"+str(results[1][i])
+    results_final.append (str(results[0][i])+":"+str(results[1][i]))
+results_final.sort()
+
+for i in range (len(results_final)): 
+    print results_final[i]
